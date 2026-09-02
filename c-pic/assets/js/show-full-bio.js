0a19875c289d86320b3eeeba12d03105d15070b2
David Lomas <david.lomas@knowinnovation.com>
2026-09-02 10:00:07 +0100

    Fix bio bug

--- c-pic/assets/js/show-full-bio.js
+++ c-pic/assets/js/show-full-bio.js
@@ -5,10 +5,12 @@
     var rest = document.getElementById('therest')
     var show = document.getElementById('showfullbio')
 
-    show.addEventListener('click', function (event) {
-      show.style.display = 'none'
-      rest.style['font-size'] = '1em'
-    })
+    if (show) {
+      show.addEventListener('click', function (event) {
+        show.style.display = 'none'
+        rest.style['font-size'] = '1em'
+      })
+    }
 
   })
 }());
